e0952d80fa30b31a02d7adf5519a57fdf740fe6f
nathankaufman <nlk7665@rit.edu>
2022-02-16 11:13:18 -0500

    added home.js

---
+++ portfolio/src/tabs/Home.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+function Home(){
+    const styles = {
+
+    }
+    
+    return(
+        <div style={styles}>
+
+        </div>
+    );
+}
+
+export default Home;
