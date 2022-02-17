d338b86770d1617ca9cec45507eb6b4ccda113c8
nathankaufman <nlk7665@rit.edu>
2022-02-17 09:58:32 -0500

    fighting conditional rendering

--- portfolio/src/tabs/Home.js
+++ portfolio/src/tabs/Home.js
@@ -28,16 +28,50 @@ function Moonfall(props){
     </div>)
 }
 
+function Defaultview(props){
+    return(<div>
+        <p>Please select a project for a description and dedicated link</p>
+    </div>)
+}
+
 function Home(props){
+    const[viewcomp, setveiwcomp] = useState(0);
     const styles = {
         'textAlign': 'center',
         'paddingLeft': '25%',
         'paddingRight': '25%'
     }
+
+    const handleSubmit = (event) => {
+        console.log("Handle sublit called")
+        console.log(event)
+        // setveiwcomp(viewcomp)
+        console.log(viewcomp)
+        event.preventDefault();
+
+    }
+
+    let view = <Defaultview />;
+
+    console.log(viewcomp);
+    if(viewcomp == 1){
+
+    }
     
     return(
         <div style={styles}>
             <p><a href='https://github.com/Natecat4444'>Github Profile</a></p>
+            <form onSubmit={handleSubmit}>
+                <select name={viewcomp} onChange={(event) => setveiwcomp(event.target.value)}>
+                    <option value={1}>Battleship</option>
+                    <option value={2}>Planner</option>
+                    <option value={3}>SteamAPI</option>
+                    <option value={4}>Moonfall</option>
+                </select>
+                <button type="submit">View Project</button>
+            </form>
+
+            {view}
 
         </div>
     );
